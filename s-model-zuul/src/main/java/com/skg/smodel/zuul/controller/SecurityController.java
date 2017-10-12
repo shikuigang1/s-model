1555f6d87e7a03c290f1894620e22b037e2ec1c3
shikuigang <slting12@163.com>
2017-10-12 08:59:51 +0800

    11

--- s-model-zuul/src/main/java/com/skg/smodel/zuul/controller/SecurityController.java
+++ s-model-zuul/src/main/java/com/skg/smodel/zuul/controller/SecurityController.java
@@ -4,13 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * ${DESCRIPTION}
- *
- * @author wanghaobin
- * @create 2017-06-02 13:43
- */
-//@Controller
+@Controller
 public class SecurityController {
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String login() {
