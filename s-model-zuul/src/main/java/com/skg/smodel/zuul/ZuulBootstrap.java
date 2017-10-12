423e9ebcb4f40aceb19f360c338ba29670658177
shikuigang <slting12@163.com>
2017-10-12 08:59:21 +0800

    11

--- s-model-zuul/src/main/java/com/skg/smodel/zuul/ZuulBootstrap.java
+++ s-model-zuul/src/main/java/com/skg/smodel/zuul/ZuulBootstrap.java
@@ -1,13 +1,10 @@
-package com.skg.smodel.zuul.gate;
+package com.skg.smodel.zuul;
 
-import com.skg.smodel.zuul.filter.SessionAccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -21,9 +18,9 @@ public class ZuulBootstrap {
         SpringApplication.run(ZuulBootstrap.class, args);
     }
 
-
+/*
     @Bean
     public SessionAccessFilter sessionAccessFilter(){
         return new SessionAccessFilter();
-    }
+    }*/
 }
