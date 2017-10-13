368a890f71d8db92af674d87b95dc28044c9fd99
shikuigang <slting12@163.com>
2017-10-13 17:38:32 +0800

    12

---
+++ s-model-core/src/main/java/com/skg/smodel/core/auth/jwt/IJWTInfo.java
@@ -0,0 +1,22 @@
+package com.skg.smodel.core.auth.jwt;
+
+
+public interface IJWTInfo {
+    /**
+     * 获取用户名
+     * @return
+     */
+    String getUniqueName();
+
+    /**
+     * 获取用户ID
+     * @return
+     */
+    String getId();
+
+    /**
+     * 获取名称
+     * @return
+     */
+    String getName();
+}
