dfd61eff47baf08d34f2ab6e30db582524f05500
shikuigang <slting12@163.com>
2017-10-13 15:39:25 +0800

    12

---
+++ s-model-core/src/main/java/com/skg/smodel/core/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.skg.smodel.core.exception.handler;
+
+import com.skg.smodel.core.constant.CommonConstants;
+import com.skg.smodel.core.exception.BaseException;
+import com.skg.smodel.core.msg.BaseResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@ControllerAdvice("com.github.wxiaoqi.security")
+@ResponseBody
+public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 自定义 异常处理
+     * @param response
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse baseExceptionHandler(HttpServletResponse response, BaseException ex) {
+        logger.error(ex.getMessage(),ex);
+        return new BaseResponse(ex.getStatus(), ex.getMessage());
+    }
+
+    /**
+     * 没有 捕捉到的异常处理
+     * @param response
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public BaseResponse otherExceptionHandler(HttpServletResponse response, Exception ex) {
+        logger.error(ex.getMessage(),ex);
+        return new BaseResponse(CommonConstants.EX_OTHER_CODE, ex.getMessage());
+    }
+}
